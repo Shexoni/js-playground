c0d49b7e133c917ee8fa9f21bfe9d77685ac66d2
Shexoni <bogdan.gft@gmail.com>
2024-03-30 14:47:36 -0400

    challenge completed

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,11 @@
-function findMaxNumber() {}
+function findMaxNumber(arr) {
+  maxNumber = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (maxNumber < arr[i]) {
+      maxNumber = arr[i];
+    }
+  }
+  return maxNumber;
+}
 
 module.exports = findMaxNumber;
